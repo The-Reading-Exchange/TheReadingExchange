a76d2fadb70cbd6a85660ff98eae2a4b734507d0
Kane Price <price.kane@gmail.com>
2018-03-04 15:55:58 -0500

    some work towarrds the initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+        { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+        { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ]),
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
