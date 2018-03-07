b6c009cc6fd81759f27cefb7fd68baed341f9c63
Kane Price <kprice@relias.com>
2018-03-07 09:49:57 -0500

    working auth

---
+++ client/src/app/profile/profile.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { AuthenticationService, UserDetails } from '../authentication.service';
+
+@Component({
+  templateUrl: './profile.component.html'
+})
+export class ProfileComponent {
+  details: UserDetails;
+
+  constructor(private auth: AuthenticationService) {}
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+    this.auth.profile().subscribe(user => {
+      this.details = user;
+    }, (err) => {
+      console.error(err);
+    });
+  }
+}
