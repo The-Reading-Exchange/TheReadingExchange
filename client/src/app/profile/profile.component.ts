88987edd581f7c8089a6e1f2b3b7fccfb7c473cc
Claire Pate <clairepate@Claires-MacBook-Air.local>
2018-03-08 20:57:48 -0500

    small changes to navbar/ homepage

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { AuthenticationService, UserDetails } from '../authentication.service';
 
 @Component({
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
   details: UserDetails;
