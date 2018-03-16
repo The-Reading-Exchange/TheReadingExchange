a76f2ff088afa43a21646c01a9db78514fdea90c
Kane Price <price.kane@gmail.com>
2018-03-15 22:51:32 -0400

    added getuserbooks

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,21 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { AuthenticationService, UserDetails } from '../authentication.service';
+import { SearchService } from '../search.service';
+
 
 @Component({
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  profileBooks;
   details: UserDetails;
 
-  constructor(private auth: AuthenticationService) {}
+  constructor(private searchService: SearchService, private auth: AuthenticationService) {}
 
   // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit() {
     this.auth.profile().subscribe(user => {
       this.details = user;
+      this.getUserBooks(this.details);
     }, (err) => {
       console.error(err);
     });
+  }
+
+  getUserBooks(user) {
+    console.log('starting the call from init');
+    this.searchService.getUserBooks(user)
+    .subscribe(
+      res => {
+        this.profileBooks = res.books;
+        console.log(this.profileBooks, 'we are done');
+      },
+      err => {
+        console.log('Error occured');
+      }
+    );
   }
 }
